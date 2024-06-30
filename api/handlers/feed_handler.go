688d84c3a2beffc791f18260f203d36f945880df
Ochieng Otieno <otieno@dialafrika.com>
2024-06-30 13:05:56 +0300

    delete page and page_size from response

--- api/handlers/feed_handler.go
+++ api/handlers/feed_handler.go
@@ -3,31 +3,17 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 
-	"github.com/ochiengotieno304/feedpulse-go/pkg/models"
 	"github.com/ochiengotieno304/feedpulse-go/pkg/stores"
 )
 
 type FeedHandler struct {
 }
 
-type newsResponse struct {
-	Page     string         `json:"page,omitempty"`
-	PageSize string         `json:"page_size,omitempty"`
-	Feeds    *[]models.News `json:"feeds,omitempty"`
-}
-
 var feedStore = stores.NewFeedStore()
 
 func (FeedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	news, err := feedStore.GetAll(r)
-
-	response := newsResponse{
-		Feeds:    news,
-		Page:     r.URL.Query().Get("page"),
-		PageSize: strconv.Itoa(len(*news)),
-	}
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -38,7 +24,7 @@ func (FeedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
-	if err := json.NewEncoder(w).Encode(&response); err != nil {
+	if err := json.NewEncoder(w).Encode(&news); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 		return
