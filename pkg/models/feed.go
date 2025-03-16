b9ed926a9ec937e16b1cb36cf9a47d1ae8a46c93
ochieng otieno <ochiengotieno71@gmail.com>
2025-03-16 15:21:29 +0300

    feat: add picture_url to feed model

--- pkg/models/feed.go
+++ pkg/models/feed.go
@@ -3,15 +3,16 @@ package models
 import "time"
 
 type News struct {
-	ID            int64  `json:"id,omitempty"`
-	Title         string `json:"title,omitempty"`
-	Snippet       string `json:"snippet,omitempty"`
-	URL           string `json:"url,omitempty"`
-	Source        string `json:"source,omitempty"`
-	Code          string `json:"country,omitempty"`
-	Category      string `json:"category,omitempty"`
-	Language      string `json:"language,omitempty"`
+	ID            int64     `json:"id,omitempty"`
+	Title         string    `json:"title,omitempty"`
+	Snippet       string    `json:"snippet,omitempty"`
+	URL           string    `json:"url,omitempty"`
+	Source        string    `json:"source,omitempty"`
+	Code          string    `json:"country,omitempty"`
+	Category      string    `json:"category,omitempty"`
+	Language      string    `json:"language,omitempty"`
 	PublishedDate time.Time `json:"published_date,omitempty"`
+	PictureURL    string    `json:"picture_url,omitempty"`
 }
 
 type Response struct {
